52b873676b77a444814fa37644c3582d4ce2bd44
daffaabo <daffa.budi2003@gmail.com>
2025-05-02 14:44:08 +0700

    Menambahkan filter untuk rute 'login' dan '/login' pada konfigurasi Filters.php. Memperbaiki komentar untuk konsistensi penulisan.

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -50,6 +50,8 @@ class Filters extends BaseConfig
                     'angkatan/*',
                     'prodi/*',
                     'api/*',
+                    'login',
+                    '/login'
                 ]
             ],
             // 'invalidchars',
@@ -70,7 +72,7 @@ class Filters extends BaseConfig
      *
      * If you use this, you should disable auto-routing because auto-routing
      * permits any HTTP method to access a controller. Accessing the controller
-     * with a method you don’t expect could bypass the filter.
+     * with a method you don't expect could bypass the filter.
      *
      * @var array
      */
